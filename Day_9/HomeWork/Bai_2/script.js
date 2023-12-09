20a3b09b87329b5c355b28aeb87d9fc5654c7c83
Ngo Duc Canh <ngoduccanh19@gmail.com>
2023-12-09 11:40:35 +0700

    feat: fix classname ex2

--- Day_9/HomeWork/Bai_2/script.js
+++ Day_9/HomeWork/Bai_2/script.js
@@ -1,4 +1,4 @@
-check_rectangle = (a, b, c) => {
+check_triangle = (a, b, c) => {
     if (a > 0 && b > 0 && c > 0) {
         if (a + b > c && b + c > a && c + a > b) {
             return true;
@@ -14,5 +14,5 @@ const c1 = 5;
 const a2 = 3;
 const b2 = 4;
 const c2 = 8;
-console.log(check_rectangle(a1, b1, c1));
-console.log(check_rectangle(a2, b2, c2));
+console.log(check_triangle(a1, b1, c1));
+console.log(check_triangle(a2, b2, c2));
